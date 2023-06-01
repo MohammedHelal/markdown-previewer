e6a30c569116dda3fff1a4c2d70944b5ba0eab66
MohammedHelal <130768563+MohammedHelal@users.noreply.github.com>
2023-06-01 21:36:33 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { marked } from "marked";
+import parse from "html-react-parser";
+import { useState } from "react";
+import "./styles.css";
+
+marked.use({
+  gfm: true,
+  breaks: true
+});
+
+export default function App() {
+  let [input, setInput] = useState(initialInput);
+
+  function handleChange(e) {
+    setInput(e.target.value);
+  }
+
+  let mark = marked.parse(input);
+
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto">
+        <div className="card m-3">
+          <div className="card-header">Editor</div>
+          <textarea
+            id="editor"
+            value={input}
+            onChange={(e) => handleChange(e)}
+            rows="10"
+            cols="20"
+          >
+            {initialInput}
+          </textarea>
+        </div>
+        <div className="card m-3">
+          <div className="card-header">Preview</div>
+          <div id="preview" className="card-body overflow-auto">
+            {parse(mark)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+let initialInput = `# Welcome to my React Markdown Previewer!
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+    - With different indentation levels.
+      - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`;
